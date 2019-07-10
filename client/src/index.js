e298bbda3354b1582d377ceb83f65c486af3ae28
Luis Arpasi <meclipse@gmail.com>
2019-07-10 01:28:26 -0500

    Fix package and api url env var

--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import 'semantic-ui/dist/semantic.min.css'
 import './index.css'
+import axios from 'axios'
 import App from './App'
+
+axios.defaults.baseURL = process.env.REACT_APP_API_URL
 
 ReactDOM.render(<App />, document.getElementById('root'))
