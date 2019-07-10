a938c245da113ec87fb9ddcf1d9460cd6203ff8f
Luis Arpasi <meclipse@gmail.com>
2019-07-10 02:11:48 -0500

    Add cors to server

--- server/src/server.js
+++ server/src/server.js
@@ -1,10 +1,12 @@
 import express from 'express'
 import routes from './routes'
+import cors from 'cors'
 
 const app = express()
 
 // Settings
 app.set('port', process.env.PORT || 5000)
+app.use(cors())
 
 // Middlewares
 app.use(express.json({ extended: false }))
